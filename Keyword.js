b68c38c7bbe308d58dd6346474196c503de906c0
Ankit Rajput <ankitrajputji00@gmail.com>
2024-01-21 10:55:59 +0530

    Let - Var - Const

---
+++ Keyword.js
@@ -0,0 +1,30 @@
+// FACTS ABOUT VAR:
+// - Var is accessible throughout the entire file.
+// - Var does not support block scope; it is function-scoped.
+// - The value of a var variable can be changed.
+
+var varible = 10;
+
+// FACTS ABOUT CONST:
+// - Const variables cannot have their values reassigned.
+// - Const follows block scope, meaning it is limited to the block (enclosed by curly braces) where it is declared.
+// - Const was introduced in ECMAScript 6 (ES6) and later versions.
+
+const constant = 200;
+
+// FACTS ABOUT LET:
+// - Let follows block scope, similar to const.
+// - The value of a let variable can be changed or reassigned.
+// - Let was introduced in ECMAScript 6 (ES6) and later versions.
+
+let letAssume = 20;  
+
+
+/ ********************************************************************************************** / 
+                    /*  Real Life Example  */
+// Var   : Think of var as a loudspeaker that talks to the whole file.
+// Const : Const is like a nameplate; once you write your name, you can't change it.
+// LEt   : Let is like a whiteboard; you can erase and write new things whenever you want.  
+
+
+
