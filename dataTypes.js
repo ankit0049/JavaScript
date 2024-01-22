a4f43b3b05aa88b9e85b61c60c588fe590171781
Ankit Rajput <ankitrajputji00@gmail.com>
2024-01-22 11:37:58 +0530

    Data Types eg. Number , String

---
+++ dataTypes.js
@@ -0,0 +1,40 @@
+  //  Java Script data Types  
+
+  //  1. Number
+  //  2. BigInt 
+  //  3. String 
+  //  4. Boolean 
+  //  5. Symbol
+  //  6. Null 
+  //  7. Undefined 
+
+  // Number 
+  let N = 87 
+  console.log(typeof  N) 
+  
+  
+  //  BigInt 
+  let B = 84n
+  console.log(typeof B) 
+
+  //String  
+  // Used for Storing the String Kind a Value
+  let Str = "Hello"
+  console.log(typeof Str)
+
+  // Symbol
+  // Symbol is Used For Creating Unique or random Symbol  
+  // Sym == Sym2  give false 
+  let Sym = Symbol('123') 
+  let Sym2 = Symbol('123') 
+
+  //Null  <- (typeof give object )
+ // Use of Null Assume We don't have temprature value so what you return if You return 0 but 0 is also temprature
+ // that's why we store null there
+
+ let temprature = null    
+
+ // Undefined 
+ let U ;
+ console.log(typeof U)
+
